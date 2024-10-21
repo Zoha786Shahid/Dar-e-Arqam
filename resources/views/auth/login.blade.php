0d17201d07c72466431cd56db0890cf5b00cc910
mfarhanabid <farhan@spectreco.com>
2024-10-21 04:38:56 -0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,10 +23,11 @@
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
                             <a href="index" class="d-inline-block auth-logo">
-                                <img src="{{ URL::asset('build/images/logo-light.png')}}" alt="" height="20">
+                                <!-- <img src="{{ URL::asset('build/images/logo-light.png')}}" alt="" height="20"> -->
+                                <!-- <img src="{{ public_path('images/school.jpeg') }}" alt="School Image" height="20"> -->
                             </a>
                         </div>
-                        <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p>
+                        <!-- <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p> -->
                     </div>
                 </div>
             </div>
@@ -39,7 +40,7 @@
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
                                 <h5 class="text-primary">Welcome Back !</h5>
-                                <p class="text-muted">Sign in to continue to Velzon.</p>
+                                <p class="text-muted">Sign in to continue to</p>
                             </div>
                             <div class="p-2 mt-4">
                                 <form action="{{ route('login') }}" method="POST">
@@ -79,7 +80,7 @@
                                         <button class="btn btn-success w-100" type="submit">Sign In</button>
                                     </div>
 
-                                    <div class="mt-4 text-center">
+                                    <!-- <div class="mt-4 text-center">
                                         <div class="signin-other-title">
                                             <h5 class="fs-13 mb-4 title">Sign In with</h5>
                                         </div>
@@ -89,7 +90,7 @@
                                             <button type="button" class="btn btn-dark btn-icon waves-effect waves-light"><i class="ri-github-fill fs-16"></i></button>
                                             <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
                                         </div>
-                                    </div>
+                                    </div> -->
                                 </form>
                             </div>
                         </div>
@@ -97,9 +98,9 @@
                     </div>
                     <!-- end card -->
 
-                    <div class="mt-4 text-center">
+                    <!-- <div class="mt-4 text-center">
                         <p class="mb-0">Don't have an account ? <a href="register" class="fw-semibold text-primary text-decoration-underline"> Signup </a> </p>
-                    </div>
+                    </div> -->
 
                 </div>
             </div>
@@ -110,7 +111,7 @@
     <!-- end auth page content -->
 
     <!-- footer -->
-    <footer class="footer">
+    <!-- <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -120,7 +121,7 @@
                 </div>
             </div>
         </div>
-    </footer>
+    </footer> -->
     <!-- end Footer -->
 </div>
 @endsection
